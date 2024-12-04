a99dde0b5f898b59ca8873572ff3292e6fb244e2
MCaius <42388548+MCaius@users.noreply.github.com>
2024-12-04 16:42:10 +0200

    Improved URL logic

--- src/po/pages/inventory.page.js
+++ src/po/pages/inventory.page.js
@@ -1,10 +1,10 @@
 import { DashboardHeader } from '../components/DashboardHeader';
 
 export default class InventoryPage {
-  url = 'https://www.saucedemo.com/inventory.html';
+  path = '/inventory.html';
   header = new DashboardHeader();
 
   async open() {
-    await browser.url(this.url);
+    await browser.url(this.path);
   }
 }
--- src/po/pages/login.page.js
+++ src/po/pages/login.page.js
@@ -1,10 +1,10 @@
 import { LoginForm } from '../components/LoginForm';
 
 export default class LoginPage {
-    url = 'https://www.saucedemo.com/';
+    path = '/';
     form = new LoginForm();
 
     async open() {
-        await browser.url(this.url);
+        await browser.url(this.path);
     }
 }
